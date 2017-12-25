8cdf1d58a81b08d939d855ae11384e5fba4f24ce
NARVIS-UECK\ueck <ulrich.eck@tum.de>
2017-12-25 14:14:27 +0100

    simplify build integration

--- build.py
+++ build.py
@@ -22,53 +22,13 @@ def get_version_from_recipe():
     return get_value_from_recipe(r'''version\s*=\s*["'](\S*)["']''').groups()[0]
 
 
-def get_default_vars():
-    username = os.getenv("CONAN_USERNAME", "ulricheck")
-    channel = os.getenv("CONAN_CHANNEL", "stable")
-    version = get_version_from_recipe()
-    return username, channel, version
-
-
-def is_ci_running():
-    return os.getenv("APPVEYOR_REPO_NAME", "") or os.getenv("TRAVIS_REPO_SLUG", "")
-
-
-def get_ci_vars():
-    reponame_a = os.getenv("APPVEYOR_REPO_NAME","")
-    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH","")
-
-    reponame_t = os.getenv("TRAVIS_REPO_SLUG","")
-    repobranch_t = os.getenv("TRAVIS_BRANCH","")
-    print(reponame_a)
-    username, _ = reponame_a.split("/") if reponame_a else reponame_t.split("/")
-    channel, version = repobranch_a.split("/") if repobranch_a else repobranch_t.split("/")
-    return username, channel, version
-
-
-def get_env_vars():
-    return get_ci_vars() if is_ci_running() else get_default_vars()
-
-
-def get_os():
-    return platform.system().replace("Darwin", "Macos")
-
 
 if __name__ == "__main__":
     name = get_name_from_recipe()
-    username, channel, version = get_env_vars()
     reference = "{0}/{1}".format(name, version)
-    upload = "https://api.bintray.com/conan/tum-ubtirack/public-conan"
-
-    remotes = ", ".join((
-        "https://api.bintray.com/conan/conan-community/conan",
-        "https://api.bintray.com/conan/tum-ubitrack/public-conan"))
 
     builder = ConanMultiPackager(
-        username=username,
-        channel=channel,
         reference=reference,
-        upload=upload,
-        remotes=remotes,
         upload_only_when_stable=True,
         stable_branch_pattern="stable/*")
 
